1d3bad6ea3271e9b8c06da3a0dbef3405d0e072b
kisen123 <123562638+kisen123@users.noreply.github.com>
2023-05-11 09:44:19 +0200

    Add files via upload

---
+++ semihardTripletMask.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 09:43:01 2023
+
+@author: Kristian Lian
+"""
+
+def getSemihardTripletMask(output1, output2, output3):
+    
+    """
+    Inputs: Tensors -> Inputs output1, output2 and output3 are batched
+    forward-propagations of an embedding model, with the dimensions
+    [batchSize, embeddingDim]
+    
+    
+    Returns: A masking vector to select the semihard triplets, with the
+    dimensions [batchsize]. To be masked in the training loop as such:
+        
+    semihardOutput = outputX[semihardMask] 
+    
+    """
+    
+    
+    # Triplet margin
+    margin=0.2
+    
+    # For testing outputs
+    # output1 = torch.randn((128,256))
+    # output2 = torch.randn((128,256))
+    # output3 = torch.randn((128,256))
+    
+    
+    # Detaching the embedded outputs from the computational graph, we wouldn't
+    # want the .backward() pass to pass through anything unwanted.
+    output1 = output1.detach()
+    output2 = output2.detach()
+    output3 = output3.detach()
+
+
+    # Calculating euclidean distance between Anchor-Positives and Anchor-Negatives
+    pdist = torch.nn.PairwiseDistance(p=2.0)
+    
+    distAP = pdist(output1, output2)
+    distAN = pdist(output1, output3)
+    
+    # Creating the semihard masks
+    mask1 = distAP < distAN
+    mask2 = distAN < (distAP + margin)
+    
+    semihardMask = mask1 * mask2
+        
+    
+    return semihardMask
